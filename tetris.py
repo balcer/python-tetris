6272a6a070fe91351d9ca41fa914641282281f00
Bartosz Balcerzak <bartosz.maciej.balcerzak@gmail.com>
2015-06-04 18:16:16 +0200

    Created function for displaing game board

--- tetris.py
+++ tetris.py
@@ -1,6 +1,15 @@
-from array import *
-
-game_board = array('i',[1,2,3,4])
-
+import sys
 
 print "Tetris"
+
+game_board = [[0 for x in range(22)] for x in range(14)]
+
+game_board[5][2] = 3
+
+def print_game_board():
+    for y in range(0, 22):
+        for x in range(0, 14):
+            sys.stdout.write(str(game_board[x][y]))
+        print ""
+
+print_game_board()
