b3233a0210eb2d7de2951998b8f266c89082f2e7
Bartosz Balcerzak <bartosz.maciej.balcerzak@gmail.com>
2015-06-06 21:45:31 +0200

    Modyfied try_to_place_block - using different aproach

--- tetris.py
+++ tetris.py
@@ -19,14 +19,62 @@ gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Tetris')
 
 def print_game_board():
+    gameDisplay.fill((0,0,0))
     pygame.draw.line(gameDisplay, (0, 0, 255), (20, 20), (20, 420), 1)
     pygame.draw.line(gameDisplay, (0, 0, 255), (20, 420), (220, 420), 1)
     pygame.draw.line(gameDisplay, (0, 0, 255), (220, 420), (220, 20), 1)
+    temp_game_board = copy.deepcopy(game_board)
+    for y in range(4):
+        for x in range(4):
+            if current_block[x][y] != 0:
+                temp_game_board[current_x_position + x][current_y_position + y] = current_block[x][y]
     for y in range(BOARD_Y_SIZE):
         for x in range(BOARD_X_SIZE):
-            if game_board[x][y] == 0 and y > 1:
-                pygame.draw.rect(gameDisplay, (255, 0 , 0), ((22*(x+1))-(x*2), (20*(y+1)-39), 18, 18), 1)
+            if temp_game_board[x][y] != 0 and y > 1:
+                pygame.draw.rect(gameDisplay, (255, 0 , 0), ((22*(x+1))-(x*2), (20*(y+1)-39), 18, 18), 0)
     pygame.display.update()
+
+def try_to_place_block(action, brick):
+    global game_board
+    global current_x_position
+    global current_y_position
+    taken_places_count = 0
+    temp_game_board = [[0 for x in range(BOARD_Y_SIZE + 2)] for x in range(BOARD_X_SIZE + 4)]
+    for y in range(BOARD_Y_SIZE + 2):
+        for x in range(BOARD_X_SIZE + 4):
+            if x < 2 or x > 11 or y > 21:
+                temp_game_board[x][y] = 9
+    for y in range(BOARD_Y_SIZE):
+        for x in range(BOARD_X_SIZE):
+            temp_game_board[x+2][y] = game_board[x][y]
+            if game_board[x][y] != 0:
+                taken_places_count = taken_places_count + 1
+    for y in range(4):
+        for x in range(4):
+            if action == 1:
+                if brick[x][y] is not 0:
+                    temp_game_board[x + current_x_position + 2][y + current_y_position + 1] = brick[x][y]
+            elif action == 2:
+                if brick[x][y] is not 0:
+                    temp_game_board[x + current_x_position + 3][y + current_y_position] = brick[x][y]
+            elif action == 3:
+                if brick[x][y] is not 0:
+                    temp_game_board[x + current_x_position + 1][y + current_y_position] = brick[x][y]
+
+    temp_border_count = 0
+    for y in range(BOARD_Y_SIZE + 2):
+        for x in range(BOARD_X_SIZE + 4):
+            if temp_game_board[x][y] == 9:
+                temp_border_count = temp_border_count + 1
+    if temp_border_count == 116:
+        print "Border OK"
+        if action == 1:
+            current_y_position = current_y_position + 1
+        elif action == 2:
+            current_x_position = current_x_position + 1
+        elif action == 3:
+            current_x_position = current_x_position - 1
+    print temp_game_board
 
 pygame.time.set_timer(TIME_EVENT, 1000)
 
@@ -52,38 +100,10 @@ while not game_exit:
 pygame.quit()
 quit()
 
-temp_game_board = [[0 for x in range(BOARD_Y_SIZE)] for x in range(BOARD_X_SIZE)]
-actual_x_position = 4
-actual_y_position = 5
-
-I_block = [[0, 0, 2, 0, 0],[0, 0, 2, 0, 0],[0, 0, 3, 0, 0],[0, 0, 2, 0, 0],[0, 0, 0, 0, 0]]
-T_block = [[0, 0, 0, 0, 0],[0, 0, 2, 0, 0],[0, 0, 3, 2, 0],[0, 0, 2, 0, 0],[0, 0, 0, 0, 0]]
-Current_block = [[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]]
-
-def try_to_place_block(block, x, y):
-    global game_board
-    global temp_game_board
-    temp_game_board = copy.deepcopy(game_board)
-    for i in range(5):
-        for j in range(5):
-            if block[j][i] != 0:
-                temp_game_board[j+x][i+y] = T_block[j][i]
-    for y in range(BOARD_Y_SIZE):
-        for x in range(BOARD_X_SIZE):
-            if x == 0 or x == 1 or x == BOARD_X_SIZE-2 or x == BOARD_X_SIZE-1 or y == BOARD_Y_SIZE-2 or y == BOARD_Y_SIZE-1:
-                if temp_game_board[x][y] != 1:
-                    return 0
-
 def screen_update():
     try_to_place_block(T_block, 5, 4)
     print_game_board()
     root.after(500, screen_update)
-
-def init_game_board():
-    for y in range(BOARD_Y_SIZE):
-        for x in range(BOARD_X_SIZE):
-            if x == 0 or x == 1 or x == BOARD_X_SIZE-2 or x == BOARD_X_SIZE-1 or y == BOARD_Y_SIZE-2 or y == BOARD_Y_SIZE-1:
-                game_board[x][y] = 1
 
 def on_key_press(event):
     pressed_key = event.char
@@ -91,5 +111,4 @@ def on_key_press(event):
         root.quit()
     text.insert('end', 'You pressed %s\n' % (event.char, ))
 
-init_game_board()
 
