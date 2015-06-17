93ef1406a41b50d534d0460c660bb1249d628307
Bartosz <bartosz.maciej.balcerzak@gmail.com>
2015-06-17 22:01:23 +0200

    Added GAME OVER functionality

--- tetris.py
+++ tetris.py
@@ -15,6 +15,8 @@ def start_game():
     global current_block_rotation
     global next_block_type
     global game_exit
+    global new_round
+    global game_over
     global gameDisplay
     game_board = [[0 for x in range(BOARD_Y_SIZE)] for x in range(BOARD_X_SIZE)]
     current_x_position = 2
@@ -23,6 +25,8 @@ def start_game():
     current_block_rotation = 0
     next_block_type = random.randint(1, 7)
     game_exit = False
+    new_round = False
+    game_over = False
     gameDisplay = pygame.display.set_mode((380, 445))
 
     pygame.init()
@@ -84,6 +88,7 @@ def start_new_round():
     global current_block_type
     global current_block_rotation
     global next_block_type
+    global new_round
     temp_block = get_block(current_block_type, current_block_rotation)
     for y in range(4):
         for x in range(4):
@@ -94,8 +99,10 @@ def start_new_round():
     current_block_rotation = 0
     current_x_position = 2
     current_y_position = 2
+    new_round = True
 
 def print_game_board():
+    global game_font
     gameDisplay.fill((0,0,0))
     label = game_font.render("Next block:", 1, (255,255,255))
     gameDisplay.blit(label, (240, 25))
@@ -117,6 +124,10 @@ def print_game_board():
         for x in range(4):
             if temp_block[x][y] != 0:
                 pygame.draw.rect(gameDisplay, (255, 0, 0), ((225 + 22*(x+1))-(x*2), (20*(y+1) + 20), 18, 18), 0)
+    if game_over == True:
+        game_font = pygame.font.SysFont("monospace", 25)
+        label = game_font.render("GAME OVER", 1, (255,255,255))
+        gameDisplay.blit(label, (120, 120))
     pygame.display.update()
 
 def try_to_place_block(action):
@@ -126,6 +137,8 @@ def try_to_place_block(action):
     global current_y_position
     global current_block_type
     global current_block_rotation
+    global new_round
+    global game_over
     temp_block_rotation = current_block_rotation + 1
     taken_places_count = 0
 
@@ -185,8 +198,11 @@ def try_to_place_block(action):
             if temp_game_board[x][y] == 8:
                 temp_taken_places_count = temp_taken_places_count + 1
     if temp_border_count == 116 and temp_taken_places_count == taken_places_count:
+
     #Taking apropiate actions
+
         if action == 1:
+            new_round = False
             current_y_position = current_y_position + 1
         elif action == 2:
             current_x_position = current_x_position + 1
@@ -196,12 +212,14 @@ def try_to_place_block(action):
             current_block_rotation = temp_block_rotation
     else:
         if action == 1:
-            start_new_round()
-
+            if new_round == True:
+                game_over = True
+            else:
+                start_new_round()
 
 start_game()
 
-while not game_exit:
+while game_exit == False and game_over == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
@@ -219,6 +237,14 @@ while not game_exit:
             elif event.key == pygame.K_ESCAPE:
                 game_exit = True
     print_game_board()
+
+while not game_exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_exit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game_exit = True
 
 pygame.quit()
 quit()
