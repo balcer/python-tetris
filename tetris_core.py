0c8c311d9e44bb5905368b941b650af78c0b0810
Bartosz Balcerzak <bartosz.balcerzak@open-rnd.pl>
2015-12-03 23:52:59 +0100

    tetris_core: added function to display raw game board (for debugging)

--- tetris_core.py
+++ tetris_core.py
@@ -29,4 +29,21 @@ class TetrisCore(object):
                 if y >= self.y_size + self.BOARD_MARGIN:
                     self.game_board[x][y] = PxType.forrbidden
 
+    def show_raw_game_board(self):
+        show_string = ''
+        for y in xrange(self.y_size + self.BOARD_MARGIN * 2):
+            for x in range(self.x_size + self.BOARD_MARGIN * 2):
+                pixel_value = self.game_board[x][y]
+                if pixel_value == 0:
+                    show_string += ' . '
+                elif pixel_value == PxType.forrbidden:
+                    show_string += ' X '
+                elif pixel_value == PxType.placed:
+                    show_string += ' O '
+                elif pixel_value == PxType.moving:
+                    show_string += ' | '
+            show_string += '\n'
+        print(show_string)
+
 my_tetris = TetrisCore(10, 22)
+my_tetris.show_raw_game_board()
