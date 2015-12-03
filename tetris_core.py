824a8e20c02b51d8a4793235ecfc0ccf716fecd2
Bartosz Balcerzak <bartosz.balcerzak@open-rnd.pl>
2015-12-03 23:50:51 +0100

    tetris_core: added game board inititialization function setting forbidden margins

--- tetris_core.py
+++ tetris_core.py
@@ -15,5 +15,18 @@ class TetrisCore(object):
         self.x_size = x_size
         self.y_size = y_size
         self.game_board = [[0 for x in range(self.y_size + self.BOARD_MARGIN * 2)] for x in range(self.x_size + self.BOARD_MARGIN * 2)]
+        self.init_game_board()
+
+    def init_game_board(self):
+        for y in xrange(self.y_size + self.BOARD_MARGIN * 2):
+            for x in range(self.x_size + self.BOARD_MARGIN * 2):
+                if x < self.BOARD_MARGIN:
+                    self.game_board[x][y] = PxType.forrbidden
+                if x >= self.x_size + self.BOARD_MARGIN:
+                    self.game_board[x][y] = PxType.forrbidden
+                if y < self.BOARD_MARGIN:
+                    self.game_board[x][y] = PxType.forrbidden
+                if y >= self.y_size + self.BOARD_MARGIN:
+                    self.game_board[x][y] = PxType.forrbidden
 
 my_tetris = TetrisCore(10, 22)
