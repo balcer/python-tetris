c33d5ce83fed283e56cbecfc7c1e8ca3c540c3de
Bartosz Balcerzak <bartosz.balcerzak@open-rnd.pl>
2015-12-03 23:44:31 +0100

    tetris_core: added basic class with constructor

---
+++ tetris_core.py
@@ -0,0 +1,10 @@
+class TetrisCore(object):
+
+    BOARD_MARGIN = 2
+
+    def __init__(self, x_size, y_size):
+        self.x_size = x_size
+        self.y_size = y_size
+        self.game_board = [[0 for x in range(self.y_size + self.BOARD_MARGIN * 2)] for x in range(self.x_size + self.BOARD_MARGIN * 2)]
+
+my_tetris = TetrisCore(10, 22)
